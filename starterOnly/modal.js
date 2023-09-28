c2dbadfccc43382831ea73a4a4213e57f84f65c6
FanLubLou <jaclermont@hotmail.com>
2023-09-28 14:42:21 +0200

    ajout et implémentation fonction closeModal (Issue#1)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,11 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+      //Issue#1 Fermer la modale, on récupère le bouton X pour fermer
+const closeBtn = document.querySelector(".close");  
+
+      //Issue#1 Fermer la modale, on écoute et on appelle sur le click la foncion closeModal
+closeBtn.addEventListener("click", closeModal)
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +23,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+      //Issue#1 Fermer la modale, close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
